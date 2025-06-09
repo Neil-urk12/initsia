0e1feeac888723dbbb5351fe648be29da858df68
Neil Vallecer <neilvallecer12@gmail.com>
2025-06-09 19:25:26 +0800

    feat: added user types and sample database structure with interface
    - refactored: db_config.ts to have option to use native mysql pooling or
    with kysely

--- src/config/db_config.ts
+++ src/config/db_config.ts
@@ -1,28 +1,51 @@
 import { createPool } from "mysql2/promise";
+import { User } from "../types/userTypes";
+import { Database } from "../types/userTypes";
+import { Kysely, MysqlDialect } from "kysely";
 
-const pool = createPool({
-  host: Bun.env.DB_HOST,
-  user: Bun.env.DB_USER,
-  password: Bun.env.DB_PASSWORD,
-  database: Bun.env.DB_NAME,
-  port: parseInt(Bun.env.DB_PORT || "3306"),
-  ...(process.env.DB_SSLMODE !== "disable" && {
-    ssl: {
-      rejectUnauthorized: true,
-    },
+// In case you prefer not to use Kysely
+// const pool = createPool({
+//   host: Bun.env.DB_HOST,
+//   user: Bun.env.DB_USER,
+//   password: Bun.env.DB_PASSWORD,
+//   database: Bun.env.DB_NAME,
+//   port: parseInt(Bun.env.DB_PORT || "3306"),
+//   ...(process.env.DB_SSLMODE !== "disable" && {
+//     ssl: {
+//       rejectUnauthorized: true,
+//     },
+//   }),
+// });
+//
+// async function testConnection() {
+//   try {
+//     await pool.getConnection();
+//     // Can execute test queries or create table if not exists here too
+//     console.log("Database connection successful");
+//   } catch (error) {
+//     console.error("Database connection failed:", error);
+//   }
+// }
+
+// await testConnection();
+//
+// export default pool;
+
+const dialect = new MysqlDialect({
+  pool: createPool({
+    host: Bun.env.DB_HOST,
+    user: Bun.env.DB_USER,
+    password: Bun.env.DB_PASSWORD,
+    database: Bun.env.DB_NAME,
+    port: parseInt(Bun.env.DB_PORT || "3306"),
+    ...(process.env.DB_SSLMODE !== "disable" && {
+      ssl: {
+        rejectUnauthorized: true,
+      },
+    }),
   }),
 });
 
-async function testConnection() {
-  try {
-    await pool.getConnection();
-    // Can execute test queries or create table if not exists here too
-    console.log("Database connection successful");
-  } catch (error) {
-    console.error("Database connection failed:", error);
-  }
-}
-
-await testConnection();
-
-export default pool;
+export const db = new Kysely<User>({
+  dialect,
+});
---
+++ src/types/userTypes.ts
@@ -0,0 +1,42 @@
+import {
+  ColumnType,
+  Generated,
+  Insertable,
+  JSONColumnType,
+  Selectable,
+  Updateable,
+} from "kysely";
+
+export interface Database {
+  users: User;
+}
+
+// Sample User interface
+// Adjust to your needs and use cases
+// I'm using camel cases for backend interface fields to closely align it with database schema
+// Adjust it depending on your project requirements and agreed conventions
+export interface User {
+  user_id: Generated<string>;
+  // full_name: string
+  first_name: string;
+  last_name: string;
+  email: string;
+  password_hash: string;
+  roles: string[];
+  created_at: ColumnType<Date, string | undefined, never>;
+  updated_at: ColumnType<Date, string | undefined, never>;
+  // metadata: JSONColumnType<{
+  //   login_at: string
+  //   last_login_at: string
+  //   last_logout_at: string     //optional
+  //   ip: string | null
+  //   // plan:
+  // }>
+}
+
+export interface UserDataForCreation {
+  full_name: string;
+  email: string;
+  password: string;
+  roles: string[];
+}
